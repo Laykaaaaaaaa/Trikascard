9e7c9068c8cf5fa8dea26a2b720055c703ebd8bf
Laykaaaaaaaa <00001114349483sp@al.educacao.sp.gov.br>
2024-11-27 13:41:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Estádio',
+    'Qual é o estádio do São Paulo?',
+    'O estádio do São Paulo é o Morumbi'
+)
+
+criaCartao(
+    'Jogadores',
+    'Quais foram os jogadores mais famosos que começaram no São Paulo ?',
+    'A lista é grande: Cafu, Kaká, Casemiro, Hernanes e Lucas Moura'
+)
+
+criaCartao(
+    'Mundial',
+    'Quantas mundiais o São Paulo tem ?',
+    'O São Paulo é o time com mais mundiais do Brasil, sendo Tricampeão'
+)
+
+criaCartao(
+    'Jogadores',
+    'Qual é o jogador mais importante do time ?',
+    'O jogador mais imporante para o São Paulo é o goleiro Rogério Ceni, um ícone entre todos os goleiros brasileiros'
+)
